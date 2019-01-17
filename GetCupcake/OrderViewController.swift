ea0ba87dfd206f98d071b1e5c0c69d4d6a03514c
Victor Yamchinov <kodiers@gmail.com>
2019-01-17 05:18:21 +0300

    added siri intents definition

--- GetCupcake/OrderViewController.swift
+++ GetCupcake/OrderViewController.swift
@@ -73,26 +73,7 @@ class OrderViewController: UIViewController {
     }
     
     func donate(_ order: Order) {
-        let activity = NSUserActivity(activityType: "com.techforline.GetCupcake.order")
-        // give it a title that will be displayed to users
-        let orderName = order.name
-        if ["A", "E", "I", "O", "U"].contains(orderName.first) {
-            activity.title = "Order an \(orderName)."
-        } else {
-            activity.title = "Order a \(orderName)."
-        }
-        // allow Siri to index this and use it for voice-matched queries
-        activity.isEligibleForSearch = true
-        activity.isEligibleForPrediction = true
         
-        let encoder = JSONEncoder()
-        if let orderData = try? encoder.encode(order) {
-            activity.userInfo = ["order": orderData]
-        }
-        activity.persistentIdentifier = NSUserActivityPersistentIdentifier(order.name)
-        // suggest a way for the user to trigger this shortcut
-        activity.suggestedInvocationPhrase = "I need a cupcake!"
-        self.userActivity = activity
     }
 
 }
